8546994e75badcbb8620c505926e3606a640e165
ehdwn4019 <ehdwn7531@gmail.com>
2021-04-19 14:58:40 +0900

    < 6장 : 닷지 / 플레이어 제작 > - 씬 구성하기 - 카메라 설정하기 - 플레이어 제작 - 플레이어 스크립트 생성 - 사용자 입력 감지 - 플레이어 사망 처리 - PlayerController 스크립트 개선하기 - 입력 매니저
    < 7장 : 닷지 / 탄알 제작 >
    - 탄알 게임 오브젝트 준비
    - 탄알 스크립트 준비
    - 탄알의 충돌 처리
    - 탄알 생성기 준비
    - 탄알 생성기 스크립트 만들기
    
    < 8장 : 닷지 / 게임 매니저와 UI , 최종 완성 >
    - 프로젝트 정리
    - 바닥 회전
    - 게임 UI 제작
    - 게임 매니저 제작
    - 빌드하기

---
+++ Chap06,07,08/Assets/Scripts/Rotator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotationSpeed = 60f;
+
+    float count = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //원래코드
+        //transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+
+        count += Time.deltaTime;
+
+        if (count>=3.0f)
+        {
+            transform.Rotate(0f, -rotationSpeed * Time.deltaTime, 0f);
+            if(count>=6.0f)
+            {
+                count = 0;
+            }
+
+        }
+        else
+        {
+            transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+        }
+    }
+}
